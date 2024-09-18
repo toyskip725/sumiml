4cddc87b456cbc364e2e7e2fc7a597f24b72428b
toyskip725 <toyskip725@gmail.com>
2024-09-18 21:15:02 +0900

    [feature] create parser type declaration

--- packages/core/src/parser/parser.ts
+++ packages/core/src/parser/parser.ts
@@ -1,36 +1,12 @@
-// Top-level interface of sumiML Parser
-class Parser {
-  constructor(config: ParserConfig) {
-
-  }
-
-  parse(document: string): SyntaxTreeNode {
-    const root: SyntaxTreeNode = {
-      type: "root",
-      children: []
-    };
-
-    if(document.includes("<SumiML />")) {
-      root.children.push({
-        type: "directive",
-        children: [],
-      });
-    }
-
-    return root;
-  }
-}
-
-interface ParserConfig {
-
-}
-
-interface SyntaxTreeNode {
-  type: string;
-  children: Array<SyntaxTreeNode>;
-}
-
-export {
-  Parser,
-  ParserConfig,
+type ParseSuccessOutput<T> = {
+  result: "success";
+  data: T;
+  rest: string;
 };
+
+type ParseFailOutput = {
+  result: "fail";
+};
+
+export type ParseOutput<T> = ParseSuccessOutput<T> | ParseFailOutput;
+export type Parser<T> = (input: string) => ParseOutput<T>;
