76539fbcc6ac7ab57f11a3ac093c6d1a4c238690
toyskip725 <toyskip725@gmail.com>
2024-09-25 23:44:10 +0900

    [feature] create openingTag parser

---
+++ packages/core/test/openingTag.test.ts
@@ -0,0 +1,64 @@
+import { test, expect, describe } from 'vitest';
+import { ParseOutput } from '../src/parser/parser';
+import openingTag, { OpeningTag } from '../src/syntax/openingTag';
+
+describe("openingTag", () => {
+  test("success1", () => {
+    // Arrange
+    const sample1 = "<Test>";
+    
+    // Act
+    const output = openingTag(sample1);
+
+    // Assert
+    expect(output).toEqual<ParseOutput<OpeningTag>>({
+      status: "success",
+      data: {
+        tagname: "Test",
+        attributes: {},
+      },
+      rest: "",
+    });
+  });
+
+  test("success2", () => {
+    // Arrange
+    const sample2 = "<Sample name=\"test2\">";
+
+    // Act
+    const output = openingTag(sample2);
+
+    // Assert
+    expect(output).toEqual<ParseOutput<OpeningTag>>({
+      status: "success",
+      data: {
+        tagname: "Sample",
+        attributes: {
+          name: "test2",
+        },
+      },
+      rest: "",
+    });
+  });
+
+  test("success3", () => {
+    // Arrange
+    const sample3 = "<Sample title=\"test3\" path=\"sample/test3\">abc</Sample>"
+
+    // Act
+    const output = openingTag(sample3);
+
+    // Assert
+    expect(output).toEqual<ParseOutput<OpeningTag>>({
+      status: "success",
+      data: {
+        tagname: "Sample",
+        attributes: {
+          title: "test3",
+          path: "sample/test3",
+        },
+      },
+      rest: "abc</Sample>",
+    });
+  });
+});
