be023376079273596fcf097af3e151c18f7b0065
toyskip725 <toyskip725@gmail.com>
2024-10-01 22:44:45 +0900

    [feature] newline parser matches repeated \r\n 2 or more than 2 times

--- packages/core/src/syntax/newline.ts
+++ packages/core/src/syntax/newline.ts
@@ -1,11 +1,12 @@
 import map from "../parser/map";
+import regexp from "../parser/regexp";
 import str from "../parser/str";
 
 export type NewLineNode = {
   type: "newline",
 };
 
-const newline = map(str("\r\n\r\n"), (_output: string) => {
+const newline = map(regexp(/^\r\n(\r\n)+/g), (_output: string) => {
   return {
     type: "newline",
   } as NewLineNode;
