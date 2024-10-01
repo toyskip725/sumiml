91a1ef64039044324ec904de961b6b3c4af8127f
toyskip725 <toyskip725@gmail.com>
2024-10-01 23:54:29 +0900

    [feature] add crlf parser

--- packages/core/src/syntax/newline.ts
+++ packages/core/src/syntax/newline.ts
@@ -1,12 +1,17 @@
 import map from "../parser/map";
 import regexp from "../parser/regexp";
-import str from "../parser/str";
 
 export type NewLineNode = {
   type: "newline",
 };
 
 const newline = map(regexp(/^\r\n(\r\n)+/g), (_output: string) => {
+  return {
+    type: "newline",
+  } as NewLineNode;
+});
+
+export const crlf = map(regexp(/^(\r\n)+/g), (_output: string) => {
   return {
     type: "newline",
   } as NewLineNode;
