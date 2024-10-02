116f75cce37643570819a6c78d0b1a4e0d120b7c
toyskip725 <toyskip725@gmail.com>
2024-10-03 00:59:00 +0900

    [feature] add new sample & test

--- packages/core/test/generator/root.test.ts
+++ packages/core/test/generator/root.test.ts
@@ -4,6 +4,9 @@ import root from '../../src/syntax/root';
 import htmlRoot from '../../src/html/root';
 import htmlBold from '../../src/html/bold';
 import htmlSection from '../../src/html/section';
+import htmlSubsection from '../../src/html/subsection';
+import htmlSubsubsection from '../../src/html/subsubsection';
+import htmlLink from '../../src/html/link';
 
 describe("root: html generator", () => {
   test("success1", async () => {
@@ -33,6 +36,42 @@ describe("root: html generator", () => {
     // Assert
     if(output.status === "success") {
       await writeFile(`${process.cwd()}/packages/core/example/sample1.html`, output.html, { encoding: 'utf8' });
+      expect(output.status).toBe("success");
+    }
+
+    expect(output.status).toBe("success");
+  });
+
+  test("success2", async () => {
+    // Arrange
+    const input = await readFile(`${process.cwd()}/packages/core/example/sample2.suml`, { encoding: 'utf8' });
+    const parser = root({
+      scope: ["Section", "Subsection", "Subsubsection"],
+      markup: ["Bold", "Link"],
+    });
+    const node = parser(input.replace(/"/g, "\""));
+    if (node.status === "fail") {
+      return;
+    }
+
+    const generator = htmlRoot({
+      scope: {
+        "Section": htmlSection,
+        "Subsection": htmlSubsection,
+        "Subsubsection": htmlSubsubsection,
+      },
+      markup: {
+        "Bold": htmlBold,
+        "Link": htmlLink,
+      }
+    });
+    
+    // Act
+    const output = generator(node.data);
+
+    // Assert
+    if(output.status === "success") {
+      await writeFile(`${process.cwd()}/packages/core/example/sample2.html`, output.html, { encoding: 'utf8' });
       expect(output.status).toBe("success");
     }
 
